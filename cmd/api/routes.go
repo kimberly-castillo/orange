d4aab26ef4d53d9665bf83ece628bbdbeb02acfc
kimberly-castillo <2021154911@ub.edu.bz>
2023-02-12 20:58:23 -0600

    new feb 12

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,1 +1,20 @@
 //Filename: cmd/api/routes.go
+
+package main
+
+import (
+	"new/http"
+
+	"github.com/julienshmidt/httprouter" //check picture on phone
+)
+
+//this function returns the router
+func (app *application) routes() *httprouter.Router {
+	//create a new router
+	router := httprouter.New()
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "v1/schools", app.createSchoolHandler)
+	router.HandlerFunc(http.MethodGet, "v1/schools/:id", app.showSchoolHandler)
+	//return the router
+	return router
+}
---
+++ cmd/api/schools.go
@@ -0,0 +1,16 @@
+//Filename: cmd/api/shcools.go
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Created a school...")
+}
+
+func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "School displayed...")
+}
