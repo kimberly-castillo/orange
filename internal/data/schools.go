99ce048cefe7b57ffc0b382f6049f452a429674c
kimberly-castillo <2021154911@ub.edu.bz>
2023-03-08 15:15:45 -0600

    "schools.go"

--- internal/data/schools.go
+++ internal/data/schools.go
@@ -1,1 +1,21 @@
 //Filename: cmd/api/schools.go
+
+package data
+
+import (
+	"time"
+)
+
+type School struct {
+	ID       int64
+	Name     string
+	Level    string
+	Contact  string
+	Phone    string
+	Email    string
+	Website  string
+	Address  string
+	Mode     []string
+	CreateAt time.Time
+	Version  int32
+}
