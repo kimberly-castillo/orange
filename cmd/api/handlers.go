d7ed159d64bdff6e88b2649a5e25b3f0bf543250
kimberly-castillo <2021154911@ub.edu.bz>
2023-03-20 15:21:46 -0600

    initial version of json response

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -11,7 +12,30 @@ import (
 )
 
 func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Created a school...")
+	//creating a struct to hold a school that will be provided to us via the request
+	//api should be able to hold post requests
+	//post (write) means you want to affect the server/db
+	//GET doesnt change the db
+	//user will send it as a json object, so we take it, store it in go program in instance of struct in order to save to db
+	var input struct {
+		Name    string   `json:"name"`
+		Level   string   `json:"level"`
+		Contact string   `json:"contact"`
+		Phone   string   `json:"phone"`
+		Email   string   `json:"email"`
+		Website string   `json:"website,omitempty"`
+		Address string   `json:"address"`
+		Mode    []string `json:"mode"`
+	}
+	//initialize a new json.Decorder
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		//decode returns error so check for error
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+	//print the request
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request) {
